12700523c062f8b25a5e16340a0332a783c2b0c9
nvanbao <154438378+nvanbao@users.noreply.github.com>
2024-10-28 23:19:48 +0700

    nguyenvanbao

---
+++ src/assignment1/Assignment1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package assignment1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Assignment1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+      
+        String ten = "Nguyen Van Bao";
+        String lop = "TPM15";
+        String maSoSinhVien = "123456789";
+
+        // In thông tin sinh viên ra màn hình
+        System.out.println("Ten: " + ten);
+        System.out.println("Lop: " + lop);
+        System.out.println("Ma so sinh vien: " + maSoSinhVien);
+    }
+}
